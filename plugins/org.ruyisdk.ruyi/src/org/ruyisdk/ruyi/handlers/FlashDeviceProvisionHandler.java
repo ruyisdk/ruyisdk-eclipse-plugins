69bde6ca9edb678db8b9f0479bdc4fcdc50899e4
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2026-02-25 15:11:47 -0500

    fix: remove unused imports
    will check this during Maven build.
    
    Signed-off-by: Mozi <29089388+pzhlkj6612@users.noreply.github.com>

--- plugins/org.ruyisdk.ruyi/src/org/ruyisdk/ruyi/handlers/FlashDeviceProvisionHandler.java
+++ plugins/org.ruyisdk.ruyi/src/org/ruyisdk/ruyi/handlers/FlashDeviceProvisionHandler.java
@@ -4,7 +4,6 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.ruyisdk.core.util.PluginLogger;
 import org.ruyisdk.ruyi.Activator;
