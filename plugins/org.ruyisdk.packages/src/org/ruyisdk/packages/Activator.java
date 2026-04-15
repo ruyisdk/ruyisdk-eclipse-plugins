2b4ffd75c4128e6214e93148a15ece52c31cce7b
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2026-04-15 14:16:39 -0400

    feat(packages): add Activator
    Signed-off-by: Mozi <29089388+pzhlkj6612@users.noreply.github.com>

---
+++ plugins/org.ruyisdk.packages/src/org/ruyisdk/packages/Activator.java
@@ -0,0 +1,41 @@
+package org.ruyisdk.packages;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.ruyisdk.core.util.PluginLogger;
+
+/**
+ * Activator for the packages plugin.
+ */
+public class Activator extends AbstractUIPlugin {
+
+    public static final String PLUGIN_ID = "org.ruyisdk.packages";
+    private static Activator plugin;
+
+    private static final PluginLogger LOGGER =
+                    new PluginLogger(Platform.getLog(FrameworkUtil.getBundle(Activator.class)), PLUGIN_ID);
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /** Returns a logger that does not depend on {@link #getDefault()} being initialized. */
+    public static PluginLogger getLogger() {
+        return LOGGER;
+    }
+
+    /** Returns the shared instance. */
+    public static Activator getDefault() {
+        return plugin;
+    }
+}
